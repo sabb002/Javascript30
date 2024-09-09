0d4d489d9a8b4a9dc24ac92f72915f6e4cc738f6
sabb <sabbirhossainalvee@gmail.com>
2024-09-09 15:49:18 +0600

    Day 7

---
+++ 07 - Array Cardio Day 2/data.js
@@ -0,0 +1,306 @@
+export const people = [
+  {
+    "ID": "a1b2c3d4",
+    "Name": "Abdus Salam",
+    "Profession": "Teacher",
+    "BirthYear": 1989,
+    "District": "Dhaka",
+    "Income": 50000
+  },
+  {
+    "ID": "e5f6g7h8",
+    "Name": "Khaled Hossain",
+    "Profession": "Athlete",
+    "BirthYear": 1997,
+    "District": "Narsingdi",
+    "Income": 30000
+  },
+  {
+    "ID": "i9j0k1l2",
+    "Name": "Ayesha Akter",
+    "Profession": "Doctor",
+    "BirthYear": 1994,
+    "District": "Chittagong",
+    "Income": 80000
+  },
+  {
+    "ID": "m3n4o5p6",
+    "Name": "Farhana Islam",
+    "Profession": "Engineer",
+    "BirthYear": 1996,
+    "District": "Khulna",
+    "Income": 60000
+  },
+  {
+    "ID": "q7r8s9t0",
+    "Name": "Mahmudul Hasan",
+    "Profession": "Lawyer",
+    "BirthYear": 1984,
+    "District": "Rajshahi",
+    "Income": 70000
+  },
+  {
+    "ID": "u1v2w3x4",
+    "Name": "Nusrat Jahan",
+    "Profession": "Nurse",
+    "BirthYear": 1992,
+    "District": "Sylhet",
+    "Income": 40000
+  },
+  {
+    "ID": "y5z6a7b8",
+    "Name": "Tanvir Ahmed",
+    "Profession": "Businessman",
+    "BirthYear": 1979,
+    "District": "Barisal",
+    "Income": 100000
+  },
+  {
+    "ID": "c9d0e1f2",
+    "Name": "Shakib Al Hasan",
+    "Profession": "Cricketer",
+    "BirthYear": 1991,
+    "District": "Dhaka",
+    "Income": 120000
+  },
+  {
+    "ID": "g3h4i5j6",
+    "Name": "Rafiq Uddin",
+    "Profession": "Farmer",
+    "BirthYear": 1974,
+    "District": "Comilla",
+    "Income": 20000
+  },
+  {
+    "ID": "k7l8m9n0",
+    "Name": "Sumaiya Rahman",
+    "Profession": "Student",
+    "BirthYear": 2002,
+    "District": "Mymensingh",
+    "Income": 10000
+  },
+  {
+    "ID": "o1p2q3r4",
+    "Name": "Shafiqul Islam",
+    "Profession": "Politician",
+    "BirthYear": 1969,
+    "District": "Dhaka",
+    "Income": 90000
+  },
+  {
+    "ID": "s5t6u7v8",
+    "Name": "Laila Begum",
+    "Profession": "Housewife",
+    "BirthYear": 1976,
+    "District": "Rangpur",
+    "Income": 15000
+  },
+  {
+    "ID": "w9x0y1z2",
+    "Name": "Imran Hossain",
+    "Profession": "Software Engineer",
+    "BirthYear": 1995,
+    "District": "Gazipur",
+    "Income": 70000
+  },
+  {
+    "ID": "a3b4c5d6",
+    "Name": "Jannatul Ferdous",
+    "Profession": "Journalist",
+    "BirthYear": 1990,
+    "District": "Narayanganj",
+    "Income": 50000
+  },
+  {
+    "ID": "e7f8g9h0",
+    "Name": "Kamal Hossain",
+    "Profession": "Banker",
+    "BirthYear": 1986,
+    "District": "Bogura",
+    "Income": 60000
+  },
+  {
+    "ID": "i1j2k3l4",
+    "Name": "Shirin Akter",
+    "Profession": "Scientist",
+    "BirthYear": 1988,
+    "District": "Pabna",
+    "Income": 80000
+  },
+  {
+    "ID": "m5n6o7p8",
+    "Name": "Arifur Rahman",
+    "Profession": "Architect",
+    "BirthYear": 1993,
+    "District": "Jessore",
+    "Income": 70000
+  },
+  {
+    "ID": "q9r0s1t2",
+    "Name": "Taslima Nasrin",
+    "Profession": "Writer",
+    "BirthYear": 1982,
+    "District": "Dhaka",
+    "Income": 60000
+  },
+  {
+    "ID": "u3v4w5x6",
+    "Name": "Mizanur Rahman",
+    "Profession": "Photographer",
+    "BirthYear": 1987,
+    "District": "Tangail",
+    "Income": 40000
+  },
+  {
+    "ID": "y7z8a9b0",
+    "Name": "Rashed Khan",
+    "Profession": "Activist",
+    "BirthYear": 1998,
+    "District": "Noakhali",
+    "Income": 30000
+  },
+  {
+    "ID": "c1d2e3f4",
+    "Name": "Fahim Ahmed",
+    "Profession": "Chef",
+    "BirthYear": 1991,
+    "District": "Feni",
+    "Income": 50000
+  },
+  {
+    "ID": "g5h6i7j8",
+    "Name": "Rubel Hossain",
+    "Profession": "Police Officer",
+    "BirthYear": 1983,
+    "District": "Kushtia",
+    "Income": 60000
+  },
+  {
+    "ID": "k9l0m1n2",
+    "Name": "Nazma Begum",
+    "Profession": "Social Worker",
+    "BirthYear": 1985,
+    "District": "Mymensingh",
+    "Income": 40000
+  },
+  {
+    "ID": "o3p4q5r6",
+    "Name": "Asif Iqbal",
+    "Profession": "Musician",
+    "BirthYear": 1996,
+    "District": "Dhaka",
+    "Income": 50000
+  },
+  {
+    "ID": "s7t8u9v0",
+    "Name": "Sharmin Sultana",
+    "Profession": "Fashion Designer",
+    "BirthYear": 1994,
+    "District": "Barisal",
+    "Income": 60000
+  },
+  {
+    "ID": "w1x2y3z4",
+    "Name": "Abul Kalam Azad",
+    "Profession": "Pilot",
+    "BirthYear": 1989,
+    "District": "Sylhet",
+    "Income": 100000
+  },
+  {
+    "ID": "a5b6c7d8",
+    "Name": "Rina Akter",
+    "Profession": "Tailor",
+    "BirthYear": 1979,
+    "District": "Mymensingh",
+    "Income": 20000
+  },
+  {
+    "ID": "e9f0g1h2",
+    "Name": "Saiful Islam",
+    "Profession": "Electrician",
+    "BirthYear": 1984,
+    "District": "Chittagong",
+    "Income": 30000
+  },
+  {
+    "ID": "i3j4k5l6",
+    "Name": "Munira Rahman",
+    "Profession": "Pharmacist",
+    "BirthYear": 1995,
+    "District": "Khulna",
+    "Income": 50000
+  },
+  {
+    "ID": "m7n8o9p0",
+    "Name": "Zakir Hossain",
+    "Profession": "Driver",
+    "BirthYear": 1974,
+    "District": "Rajshahi",
+    "Income": 25000
+  },
+  {
+    "ID": "q1r2s3t4",
+    "Name": "Shahana Begum",
+    "Profession": "Librarian",
+    "BirthYear": 1992,
+    "District": "Dhaka",
+    "Income": 40000
+  },
+  {
+    "ID": "u5v6w7x8",
+    "Name": "Anwar Hossain",
+    "Profession": "Plumber",
+    "BirthYear": 1986,
+    "District": "Narayanganj",
+    "Income": 30000
+  },
+  {
+    "ID": "y9z0a1b2",
+    "Name": "Shafiqur Rahman",
+    "Profession": "Carpenter",
+    "BirthYear": 1980,
+    "District": "Gazipur",
+    "Income": 35000
+  },
+  {
+    "ID": "c3d4e5f6",
+    "Name": "Rasheda Khatun",
+    "Profession": "Gardener",
+    "BirthYear": 1977,
+    "District": "Bogura",
+    "Income": 20000
+  },
+  {
+    "ID": "g7h8i9j0",
+    "Name": "Mahbub Alam",
+    "Profession": "Mechanic",
+    "BirthYear": 1988,
+    "District": "Pabna",
+    "Income": 40000
+  },
+  {
+    "ID": "k1l2m3n4",
+    "Name": "Shirin Sultana",
+    "Profession": "Receptionist",
+    "BirthYear": 1993,
+    "District": "Khulna",
+    "Income": 30000
+  },
+  {
+    "ID": "o5p6q7r8",
+    "Name": "Farid Uddin",
+    "Profession": "Security Guard",
+    "BirthYear": 1974,
+    "District": "Tangail",
+    "Income": 25000
+  },
+  {
+    "ID": "s9t0u1v2",
+    "Name": "Nasrin Akter",
+    "Profession": "Beautician",
+    "BirthYear": 1996,
+    "District": "Noakhali",
+    "Income": 30000
+  }
+]
